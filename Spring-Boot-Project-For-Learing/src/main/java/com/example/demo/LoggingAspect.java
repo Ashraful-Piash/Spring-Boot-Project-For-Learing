06d521a7323359e71f8b0002baeb11c27b159d58
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-10-11 19:15:48 +0600

    Update LoggingAspect.java

--- Spring-Boot-Project-For-Learing/src/main/java/com/example/demo/LoggingAspect.java
+++ Spring-Boot-Project-For-Learing/src/main/java/com/example/demo/LoggingAspect.java
@@ -3,11 +3,9 @@ package com.example.demo;
 import org.junit.Before;
 import org.springframework.stereotype.Component;
 
-@Aspect
 @Component
 public class LoggingAspect {
 
-    @Before("execution(* demo.ShoppingCart.checkout())")
     public void logger () {
         System.out.println("Loggers");
     }
